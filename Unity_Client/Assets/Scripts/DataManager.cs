58a68c9fd4c17993dab5b4468f5cb67ad2573e7b
jieshilyn <jeslynlxy@hotmail.com>
2022-03-05 18:29:39 +0800

    linkers and quiz quick back up

---
+++ Unity_Client/Assets/Scripts/DataManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // can load scene
+public class DataManager : MonoBehaviour
+{
+    public RoundData LatestRoundData;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // DontDestoryOnLoad (gameObject); // persistency
+        SceneManager.LoadScene("StudentWelcomeUI");
+    }
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public RoundData GetCurrentRoundData() {
+        return LatestRoundData;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
