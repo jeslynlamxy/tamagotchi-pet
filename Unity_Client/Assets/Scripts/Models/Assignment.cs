88d4957e52604b1b34b5875241352f7c24bc9494
Yakun212 <51481224+syakun0212@users.noreply.github.com>
2022-03-14 01:31:09 +0800

    Modified model #2

--- Unity_Client/Assets/Scripts/Models/Assignment.cs
+++ Unity_Client/Assets/Scripts/Models/Assignment.cs
@@ -5,11 +5,22 @@ using UnityEngine;
 [System.Serializable]
 public class Assignment : MonoBehaviour
 {
+    public Assignment(int assignmentId, int totalMark, string assignmentName, string assignmentTopic, string assignmentDescriptions, List<Question>questionList, List<Student> targetStudentList, int dueYear, int dueMonth, int dueDate){ 
+        this.assignmentId = assignmentId;
+        this.totalMark = totalMark;
+        this.assignmentName = assignmentName;
+        this.assignmentTopic = assignmentTopic;
+        this.assignmentDescriptions = assignmentDescriptions;
+        this.questionList = questionList;
+        this.targetStudentList = targetStudentList;
+        this.deuDate = new DateTime(Year, Month, Date, 23, 59, 00);
+    }
     public int assignmentId { get; set; }
     public int totalMark { get; set; }
     public string assignmentName {get; set; }
     public string assignmentTopic { get; set; }
-    public string assignemtnDescriptions {get; set;}    
+    public string assignmentDescriptions {get; set;}    
+    public 
     
     public List<Question> questionList { get; set; } // get from backend
     public List<Student> targetStudentList { get; set; } // get from backend
--- Unity_Client/Assets/Scripts/Models/CustomGame.cs
+++ Unity_Client/Assets/Scripts/Models/CustomGame.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 [System.Serializable]
 public class CustomGame : MonoBehaviour
 {
+
+   public CustomGame(int customeGameId, string customeGameName, string customGameDescription, List<Student> shareStudentList, List<Question> questionList){
+      this.customeGameId = customeGameId;
+      this.customeGameName = customeGameName;
+      this.customGameDescription = customGameDescription;
+      this.shareStudentList = shareStudentList;
+      this.questionList = questionList;
+   }
    public int customeGameId { get; set; }
    public string customeGameName { get; set; }
    public string customGameDescription { get; set; }
