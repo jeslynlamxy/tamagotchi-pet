c3d467ecde5ea3bf970d54b08c860a4f7871d248
weehsuan <weehsuan45@gmail.com>
2022-03-22 23:12:47 +0800

    Update

--- Unity_Client/Assets/Scripts/Models/QuestionManager.cs
+++ Unity_Client/Assets/Scripts/Models/QuestionManager.cs
@@ -7,39 +7,71 @@ using UnityEngine.SceneManagement;
 using SimpleJSON;
 using System.Linq;
 using UnityEngine.Networking;
-using Newtonsoft.Json;
 
 // Handles GET, POST, PUT and DELETE requests for Story Mode Questions
-public class QuestionManager : MonoBehaviour
-{
-    public static Boolean getQDone;
-    public static Boolean updateQDone;
-    public static Boolean addQDone;
-    public static Boolean deleteQDone;
-    public static JSONNode jsonNodeQ;
-    public static string selectedWorld = World_Select_Script.worldChoice;
-    public static string selectedSectionUnformat = Section_Select_Script.sectionChoice;
-    public static string selectedSection = selectedSectionUnformat.Replace(" ", "%20");
+public class QuestionManager : MonoBehaviour{
     public Question question;
     private HttpManager http;
+    public static Boolean getStoryQDone;
+    public static Boolean deleteStoryQDone;
+    public static Boolean updateStoryQDone;
+    public static JSONNode jsonNodeStoryQ;
+
+    public QuestionManager()
+    {
+        getStoryQDone = false;
+        deleteStoryQDone = false;
+        updateStoryQDone = false;
+    }
+
 
     // Gets story mode question by its ID
-    public void getQ(Question Q)
+    public IEnumerator getStoryQ(string questionId)
     {
-
-        http = new HttpManager();
-        var url = "http://172.21.148.165/get_question_filtered?world=" + selectedWorld + "&section=" + selectedSection; // add query parameter using username
-        var responseStr = http.Post(url, "");
-        question = JsonConvert.DeserializeObject<Question>(responseStr);
+        getStoryQDone = false;
+        API_Connection api = new API_Connection();
+        yield return StartCoroutine(api.GetData("get_question_byid?question_id=" + questionId, null, s => {
+            jsonNodeStoryQ = JSON.Parse(s);
+        }));
+        getStoryQDone = true;
     }
 
-    // Adds a story mode question
-    public void addQ(Question Q)
+
+
+    // Updates an existing story mode question
+    public void updateStoryQ(Question question)
     {
         http = new HttpManager();
-        var url = "http://172.21.148.165/add_question"; // add query parameter using username
-        var responseStr = http.Post(url, question);
-        Debug.Log(responseStr);
+        //scene = new SceneLoaderManager();
+        var url = "http://172.21.148.165/update_question";
+        var response = http.Post(url, question);
+        Debug.Log(response);
     }
 
+
+
+    public void addStoryQ(Question question)
+    {
+            http = new HttpManager();
+            //scene = new SceneLoaderManager();
+            var url = "http://172.21.148.165/add_question";
+            var response = http.Post(url, question);
+            Debug.Log(response);
+            //response = response.Substring(1, response.Length - 2);
+
+        }
+
+    // Deletes a story mode question
+    public IEnumerator deleteStoryQ(Question storyModeQ)
+    {
+        deleteStoryQDone = false;
+        API_Connection conn = new API_Connection();
+        yield return StartCoroutine(conn.DeleteData("StoryModeQuestion/" + storyModeQ.questionId, s => {
+            Debug.Log(JSON.Parse(s));
+        }));
+        deleteStoryQDone = true;
+    }
 }
+
+    
+
