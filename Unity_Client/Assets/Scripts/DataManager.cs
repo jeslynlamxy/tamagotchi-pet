ba1782295aba09b59faa7e7f649bb536ad9c806e
jieshilyn <jeslynlxy@hotmail.com>
2022-04-03 00:07:15 +0800

    push pull

--- Unity_Client/Assets/Scripts/DataManager.cs
+++ Unity_Client/Assets/Scripts/DataManager.cs
@@ -36,4 +36,11 @@ public class DataManager : MonoBehaviour
         string uniqueSessionId = ticks.ToString() + '-' + guid; //guid created by combining ticks 
         return uniqueSessionId;
     }
+    private static Random random = new Random();
+    public static string RandomString(int length)
+    {
+        const string chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+        return new string(Enumerable.Repeat(chars, length)
+        .Select(s => s[random.Next(s.Length)]).ToArray());
+    }
 }
